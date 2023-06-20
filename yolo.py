16eed2dfbbe865bac54b387be518b8025ab45466
asu2304 <aashu.patidar9876@gmail.com>
2023-06-20 19:16:26 +0530

    commit 2

---
+++ yolo.py
@@ -0,0 +1,42 @@
+from ultralytics import YOLO
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+
+model = YOLO('../Yolo-Weights/yolov8n.pt')
+# results = model("download.jpeg", show = True)
+# cv2.waitKey(0)
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    results = model(img, stream = True)
+    for r in results: 
+        boxes = r.boxes
+        for box in boxes:
+            
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # print(x1, y1, x2, y2)
+            # cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255), 3)
+            
+            w, h = x2-x1, y2-y1
+            cvzone.cornerRect(img, (x1,y1,w,h))
+            
+            
+    
+    
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+    
+    
+
+
+
+
+
+
+
