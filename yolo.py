6d4bae86d4543e4df4cb59eaf67d463a3934335a
asu2304 <aashu.patidar9876@gmail.com>
2023-06-20 20:13:37 +0530

    updated
    updated the anchor box with confidence and class name.

--- yolo.py
+++ yolo.py
@@ -1,9 +1,24 @@
 from ultralytics import YOLO
 import cv2
+import cvzone
+import math
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(3, 800)
+cap.set(4, 200)
+
+classes = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+    "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball",
+    "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle",
+    "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich",
+    "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+    "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote",
+    "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book",
+    "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+]
 
 
 model = YOLO('../Yolo-Weights/yolov8n.pt')
@@ -26,11 +41,23 @@ while True:
             w, h = x2-x1, y2-y1
             cvzone.cornerRect(img, (x1,y1,w,h))
             
+            # confidence 
+            conf = math.ceil((box.conf[0]*100)) / 100
             
-    
-    
+            # class name 
+            cls = int(box.cls[0])
+            
+            
+            class_name = classes[cls]
+            confidence = str(conf)
+
+            cvzone.putTextRect(img, f"{class_name}:{confidence}", (max(0, x1), max(35, y1)), scale = 0.7, thickness = 1)
+            # conf = box.conf[0]
+            # print(conf)
+            
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+    
     
     
 
